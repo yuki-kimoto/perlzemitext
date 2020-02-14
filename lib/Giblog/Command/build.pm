af9dc32e2a81f5910525de32a466a86b94f577e4
面川泰明 <omokawa@senk-inc.co.jp>
2020-02-14 11:08:16 +0900

    trim whitespace

--- lib/Giblog/Command/build.pm
+++ lib/Giblog/Command/build.pm
@@ -10,23 +10,23 @@ use File::Basename 'basename';
 
 sub run {
   my ($self, @args) = @_;
-  
+
   # API
   my $api = $self->api;
-  
+
   # Read config
   my $config = $api->read_config;
-  
+
   # Copy static files to public
   $api->copy_static_files_to_public;
 
   # Get files in templates directory
   my $files = $api->get_templates_files;
-  
+
   for my $file (@$files) {
-    
+
     my $data = {file => $file};
-    
+
     # Get content from file in templates directory
     $api->get_content($data);
 
@@ -53,7 +53,7 @@ sub run {
 
     # Read common templates
     $api->read_common_templates($data);
-    
+
     # Add meta title
     $api->add_meta_title($data);
 
@@ -62,10 +62,10 @@ sub run {
 
     # Build entry html
     $api->build_entry($data);
-    
+
     # Build whole html
     $api->build_html($data);
-    
+
     # Write to public file
     $api->write_to_public_file($data);
   }
@@ -77,20 +77,20 @@ sub run {
 # Create all entry list page
 sub create_list {
   my $self = shift;
-  
+
   # API
   my $api = $self->api;
-  
+
   # Config
   my $config = $api->config;
-  
+
   # Template files
   my @template_files = glob $api->rel_file('templates/blog/*');
   @template_files = reverse sort @template_files;
 
   # Data
   my $data = {file => 'list.html'};
-  
+
   # 記事一覧
   {
     my $content;
@@ -105,7 +105,7 @@ EOS
       my ($year, $month, $mday) = $base_name =~ /^(\d{4})(\d{2})(\d{2})/;
       $month =~ s/^0//;
       $mday =~ s/^0//;
-      
+
       # Year
       if ($year != $before_year) {
         $content .= <<"EOS";
@@ -115,25 +115,25 @@ EOS
 EOS
       }
       $before_year = $year;
-      
+
       # File
       my $file_entry = "blog/$base_name";
-      
+
       # Data
       my $data_entry = {file => $file_entry};
-      
+
       # Get content
       $api->get_content($data_entry);
-      
+
       # Parse title from first h tag
       $api->parse_title_from_first_h_tag($data_entry);
-      
+
       # Title
       my $title = $data_entry->{title};
       unless(defined $title) {
         $title = 'No title';
       }
-      
+
       # Add list
       $content .= <<"EOS";
   <li style="list-style:none">
@@ -142,17 +142,17 @@ EOS
 EOS
     }
     $content .= "</ul>\n";
-    
+
     # Set content
     $data->{content} = $content;
   }
-  
+
   # Add page link
   $api->add_page_link_to_first_h_tag($data);
 
   # Title
   $data->{title} = "記事一覧 - $config->{site_title}";
-  
+
   # Description
   $data->{description} = "$config->{site_title}の記事一覧";
 
@@ -167,10 +167,10 @@ EOS
 
   # Build entry html
   $api->build_entry($data);
-  
+
   # Build whole html
   $api->build_html($data);
-  
+
   # Write content to public file
   my $public_file = $api->rel_file('public/list.html');
   $api->write_to_file($public_file, $data->{content});
